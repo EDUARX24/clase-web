c2e72bd74508191773d56576971c49506015a7ce
Eduar García <86901461+EDUARX24@users.noreply.github.com>
2023-03-07 12:00:25 -0600

    Add files via upload

--- clase-web-main/clase-web-main/app.py
+++ clase-web-main/clase-web-main/app.py
@@ -1,19 +1,16 @@
 import os
+from flask import Flask, render_template, request
 from sqlalchemy import create_engine, text
-from sqlalchemy.orm import sessionmaker, scoped_session
-from dotenv import load_dotenv
-load_dotenv()
+from sqlalchemy.orm import scoped_session, sessionmaker
 
-if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
+app = Flask(__name__)
 
-#Set up Datanase
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-nombre = "Lia"
-apellido = "Poveda"
 
-consulta = text("INSERT INTO usuarios (nombre, apellido) VALUES(:nombre, :apellidos)")
-db.execute(consulta, {"nombre": nombre, "apellidos":apellido})
-db.commit()
+@app.route("/")
+def index():
+    query =  text("SELECT * FROM flights")
+    flights = db.execute(query).fetchall()
+    return render_template("index.html", flights=flights)
